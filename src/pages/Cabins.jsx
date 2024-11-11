5ef9902f4027e639d501fb0d83b540ef0a3b6aff
Arpit Jana <arpitjana2103@gmail.com>
2024-11-11 20:09:40 +0530

    setting up supabase

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,13 +1,21 @@
+import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import { getCabins } from "../services/apiCabins";
 
 function Cabins() {
-  return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
-  );
+    useEffect(function () {
+        getCabins().then(function (data) {
+            console.log(data);
+        });
+    }, []);
+
+    return (
+        <Row type="horizontal">
+            <Heading as="h1">All cabins</Heading>
+            <p>TEST</p>
+        </Row>
+    );
 }
 
 export default Cabins;
